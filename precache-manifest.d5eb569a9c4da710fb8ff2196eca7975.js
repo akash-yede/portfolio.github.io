09d18a9b05cf819bc2e714b622c08cec9fbe551e
Akash Nivrutti Yede <ayakashyede@gmail.cm>
2020-12-24 13:34:58 -0500

    Updates

---
+++ precache-manifest.d5eb569a9c4da710fb8ff2196eca7975.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6743dd0ff99cf9cb7e40b90133a00d29",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "df4193f608a994165ab2",
+    "url": "/portfolio/static/css/main.022eb82c.chunk.css"
+  },
+  {
+    "revision": "da6fd7a02cd995498cb5",
+    "url": "/portfolio/static/js/2.8e73fc4f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/portfolio/static/js/2.8e73fc4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df4193f608a994165ab2",
+    "url": "/portfolio/static/js/main.416336a5.chunk.js"
+  },
+  {
+    "revision": "c3fe483896071cc46d75",
+    "url": "/portfolio/static/js/runtime-main.75c69042.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/portfolio/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/portfolio/static/media/resume.9d3da0e2.pdf"
+  }
+]);
